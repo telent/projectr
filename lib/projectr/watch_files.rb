c691d2d96afbd76098e638c8a10c7a0a16a03be2
Daniel Barlow <dan@telent.net>
2011-05-19 18:15:07 +0100

    spec for watching-for-changes support

---
+++ lib/projectr/watch_files.rb
@@ -0,0 +1,48 @@
+# extend the project with a #watch_changes method to notify us 
+# when the files comprising it change
+
+if RUBY_PLATFORM.match(/linux/) then
+  require "rb-inotify"
+  
+  class Projectr::FileChangeWatcher < INotify::Notifier
+    def initialize(files)
+      super()
+      @files=files
+      @files.map{|x| File.dirname(x.name) }.uniq.each do |dir|
+        self.watch(dir,:close_write,:attrib)
+      end
+    end
+
+    # #to_io is required to be implemented; we inherit it from superclass
+
+    # return a list of changed files.  This interface may change: I am not 
+    # sure if a list of Deps would be more useful
+    def changed_files
+      a=[self.to_io]
+      # don't block
+      changes=[]
+      if IO.select(a,a,a,0) then
+        self.read_events.each do |e| 
+          if @files.map(&:name).member?(e.absolute_name) then
+            changes << e.absolute_name 
+          end
+        end
+      end
+      changes
+    end
+  end
+end
+
+Projectr.const_get(:FileChangeWatcher) or
+  raise "Projectr::FileChangeWatcher not implemented for this platform"
+
+class Projectr::Project
+  # Returns some object that responds to #to_io and to #changed_files.
+  # #to_io is an IO object that becomes ready for io when one or more
+  # of the project source files have been changed, and so can be 
+  # checked as pat of an application event loop.  #changed_files
+  # is an array of the files in which changes were detected
+  def watch_changes(&blk)
+    Projectr::FileChangeWatcher.new(@source_files,&blk)
+  end
+end
--- spec/projectr_spec.rb
+++ spec/projectr_spec.rb
@@ -16,4 +16,26 @@ describe Projectr::Project do
     p1.should == p2
     p1.load!
   end
+  it "detects filesystem changes" do
+    require 'projectr/watch_files'
+    w=p.watch_changes
+    w.changed_files.should be_empty
+    require 'fileutils'
+    # XXX pity the poor sod who needs to build this using a read-only
+    # source tree.
+    FileUtils.touch(p.source_files[1].name)
+    sleep 0.1
+    w.changed_files.should include p.source_files[1].name
+
+    # check the io handle thingy works
+    start=Time.now
+    Thread.new do
+      sleep 2
+      FileUtils.touch(p.source_files[1].name)
+    end
+    IO.select([w.to_io],[],[],15)
+    elapsed=Time.now-start
+    elapsed.should be > 1
+    elapsed.should be < 14
+  end
 end
